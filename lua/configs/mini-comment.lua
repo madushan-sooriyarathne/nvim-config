b65709eb2b64be909957f3c930f009135faacfaf
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2024-10-14 22:22:22 +0530

    enabled ts_context_commentstring

--- lua/configs/mini-comment.lua
+++ lua/configs/mini-comment.lua
@@ -2,10 +2,9 @@ local M = {}
 
 M.options = {
   options = {
-    --		custom_commentstring = function()
-    ---@diagnostic disable-next-line: missing-parameter
-    --			return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
-    --		end,
+    custom_commentstring = function()
+      return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+    end,
   },
 }
 
