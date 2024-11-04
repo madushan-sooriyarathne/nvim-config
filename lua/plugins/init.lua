ccbd2100712d1257e6ac8d0e2371eea988dfdc71
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2024-11-04 05:36:19 +0530

    feat: add avatnt.nvim -cusrsor like ai code assistant

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -60,6 +60,47 @@ return {
     config = true,
   },
 
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false,
+    opts = {},
+    build = "make",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+
   -- Editor Related Plugins
   { "lewis6991/gitsigns.nvim", opts = require("configs.gitsigns").options, config = true },
   {
