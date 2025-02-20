d17e2e96202230f6a7805f3fa5120d88aa3e4bc8
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2025-02-21 03:54:35 +0530

    configured snippets with luaSnip

--- lua/configs/nvim-cmp.lua
+++ lua/configs/nvim-cmp.lua
@@ -6,7 +6,17 @@ M.options = function(_, opts)
     format_kinds(entry, item) -- add icons
     return require("tailwindcss-colorizer-cmp").formatter(entry, item)
   end
-  opts.sources = vim.list_extend(opts.sources, { { name = "supermaven" } })
+  opts.snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  }
+  opts.sources = vim.list_extend(opts.sources, { { name = "supermaven" }, { name = "luasnip" } })
+end
+
+M.config = function(_, opts)
+  require("luasnip.loaders.from_vscode").lazy_load { paths = { "./snippets/typescript" } }
+  require("cmp").setup(opts)
 end
 
 return M
