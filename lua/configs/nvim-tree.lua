15a27ed44bd0ec0b5148c983f5eceb3ca0cb78d8
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2025-02-25 23:32:54 +0530

    fix: fix root and cwd being changed when navigated to a monorepo package file

--- lua/configs/nvim-tree.lua
+++ lua/configs/nvim-tree.lua
@@ -7,7 +7,15 @@ M.options = {
   git = {
     enable = true,
   },
-
+  sync_root_with_cwd = false,
+  update_focused_file = {
+    enable = false,
+    update_root = {
+      enable = false,
+      ignore_list = {},
+    },
+    exclude = false,
+  },
   renderer = {
     highlight_git = true,
     icons = {
